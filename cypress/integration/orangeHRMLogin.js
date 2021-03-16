bf589e4675283071040e7d3f699b2d16a74b1e76
Martine <martine.test@orange.fr>
2021-03-16 10:26:42 +0100

    orangeHRMLogin LOGIN panel changed to LOGIN

--- cypress/integration/orangeHRMLogin.js
+++ cypress/integration/orangeHRMLogin.js
@@ -2,7 +2,7 @@ describe('OrangeHRMLogin', function() {
 
     it('Le panneau LOGIN doit apparaître quand on ouvre l\'URL', function() {
         cy.visit('https://opensource-demo.orangehrmlive.com/')
-        cy.get('#frmLogin').contains('LOGIN Panel')
+        cy.get('#frmLogin').contains('LOGIN')
     })
 
 })
